0f47b74a0079977191250ac29acb2a8ba9e4af9a
AryanK1511 <aryankhurana1511@gmail.com>
2024-05-11 00:20:00 -0400

    AWS configuration

--- aws_config.js
+++ aws_config.js
@@ -1,21 +1,21 @@
-import { Amplify, Auth } from 'aws-amplify';
+import { Amplify } from 'aws-amplify';
 
 // Configure AWS to work with this web app
 Amplify.configure({
   Auth: {
     Cognito: {
-      userPoolId: process.env.AWS_COGNITO_POOL_ID,
-      userPoolClientId: process.env.AWS_COGNITO_CLIENT_ID,
+      userPoolId: process.env.NEXT_PUBLIC_AWS_COGNITO_POOL_ID,
+      userPoolClientId: process.env.NEXT_PUBLIC_AWS_COGNITO_CLIENT_ID,
       loginWith: {
         oauth: {
-          domain: process.env.AWS_COGNITO_HOSTED_UI_DOMAIN,
+          domain: process.env.NEXT_PUBLIC_AWS_COGNITO_HOSTED_UI_DOMAIN,
           scopes: [
             'phone',
             'email',
             'openid',
           ],
-          redirectSignIn: [process.env.OAUTH_SIGN_IN_REDIRECT_URL],
-          redirectSignOut: [process.env.OAUTH_SIGN_OUT_REDIRECT_URL],
+          redirectSignIn: [process.env.NEXT_PUBLIC_OAUTH_SIGN_IN_REDIRECT_URL],
+          redirectSignOut: [process.env.NEXT_PUBLIC_OAUTH_SIGN_OUT_REDIRECT_URL],
           responseType: 'code'
         }
       }
--- components/CustomButton/CustomButton.jsx
+++ components/CustomButton/CustomButton.jsx
@@ -2,9 +2,9 @@ import React from "react";
 import { Button } from "@radix-ui/themes";
 
 // ===== CUSTOM BUTTON COMPONENT =====
-export const CustomButton = ({ text, variant }) => {
+export const CustomButton = ({ text, variant, onClick }) => {
     return (
-        <Button variant={variant} size="3">
+        <Button variant={variant} size="3" onClick={onClick}>
             <span className="font-semibold">{ text }</span>
         </Button>
     );
