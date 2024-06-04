905556c4fc50219e38c6bf3c34b1e0795beefe9c
AryanK1511 <aryankhurana1511@gmail.com>
2024-06-04 18:50:11 -0400

    Add page to show the authenticated user

--- components/LoggedInHomepage/LoggedInHomepage.jsx
+++ components/LoggedInHomepage/LoggedInHomepage.jsx
@@ -1,6 +1,8 @@
 import React from 'react';
 import { useAtom } from 'jotai';
 import { userAtom } from '@/store';
+import { AddFragmentModal } from '../AddFragmentModal/AddFragmentModal';
+import { FragmentsList } from '../FragmentsList/FragmentsList';
 import styles from './LoggedInHomepage.module.css';
 
 // ===== LOGGED IN HOMEPAGE COMPONENT =====
@@ -9,14 +11,26 @@ export const LoggedInHomepage = () => {
   const [userDetails] = useAtom(userAtom);
 
   return (
-    <div className={`flex flex-col  mt-20 p-20 ${styles.homepage}`}>
-      <h1 className="text-6xl font-semibold">
-        Hey, 👋 <span>{userDetails.user?.username}</span>
-      </h1>
-      <p className="mt-4 text-xl text-gray-700">
-        Welcome to your dashboard. Here, you can see all of the fragments that were created under
-        your ID.
-      </p>
+    <div className={`flex flex-col  mt-16 px-12 ${styles.homepage}`}>
+      <div className="grid grid-cols-1 md:grid-cols-3 gap-4">
+        <div className="p-4 col-span-3 md:col-span-2">
+          <div>
+            <h1 className="text-6xl font-semibold">
+              Hey, 👋 <span className="text-cyan-700">{userDetails.user?.username}</span>
+            </h1>
+            <p className="mt-8 text-xl text-gray-700">
+              Welcome to your dashboard. Here, you can see all of the fragments that were created
+              under your ID and also create new ones.
+            </p>
+          </div>
+        </div>
+        <div className="col-span-1 pb-20">
+          <AddFragmentModal userDetails={userDetails} />
+        </div>
+      </div>
+      <div>
+        <FragmentsList />
+      </div>
     </div>
   );
 };
