12f003fb86ef4529e927b2d4b266e85d3d486cd4
AryanK1511 <aryankhurana1511@gmail.com>
2024-06-04 18:58:49 -0400

    Create homepage

--- app/page.js
+++ app/page.js
@@ -1,18 +1,20 @@
 'use client';
 
-import React, { useEffect } from 'react';
+import React, { useEffect, useState } from 'react';
 import { useAuth } from '@/hooks';
 import { Nav } from '@/components/Navbar/Navbar';
 import { useAtom } from 'jotai';
 import { userAtom } from '@/store';
 import { BasicHomepage } from '@/components/BasicHomepage/BasicHomepage';
-import '@/aws/config';
 import { LoggedInHomepage } from '@/components/LoggedInHomepage/LoggedInHomepage';
+import { Spinner } from '@nextui-org/react';
+import '@/aws/config';
 
 // ===== HOME PAGE =====
 export default function Home() {
   // Set state to see if the user is authenticated and store the user details in the state
   const [userDetails, setUserDetails] = useAtom(userAtom);
+  const [loading, setLoading] = useState(true);
 
   // Get user state from useAuth hook
   const { user } = useAuth();
@@ -24,12 +26,22 @@ export default function Home() {
         isLoggedIn: true,
         user: user,
       });
+
+    setLoading(false);
   }, [setUserDetails, user]);
 
   return (
     <div>
       <Nav />
-      {userDetails.isLoggedIn ? <LoggedInHomepage /> : <BasicHomepage />}
+      {loading ? (
+        <div className="absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2">
+          <Spinner label="Loading..." size="lg" />
+        </div>
+      ) : userDetails.isLoggedIn ? (
+        <LoggedInHomepage />
+      ) : (
+        <BasicHomepage />
+      )}
     </div>
   );
 }
