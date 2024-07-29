d3e5aeb6e6487ee3b21c79ac306a91d5fc7b4b2b
AryanK1511 <aryankhurana1511@gmail.com>
2024-07-29 10:57:22 -0400

    Refactor home page to use hook instead of global state

--- app/page.js
+++ app/page.js
@@ -3,8 +3,6 @@
 import React, { useEffect, useState } from 'react';
 import { useAuth } from '@/hooks';
 import { Nav } from '@/components/Navbar/Navbar';
-import { useAtom } from 'jotai';
-import { userAtom } from '@/store';
 import { BasicHomepage } from '@/components/BasicHomepage/BasicHomepage';
 import { LoggedInHomepage } from '@/components/LoggedInHomepage/LoggedInHomepage';
 import { Spinner } from '@nextui-org/react';
@@ -12,36 +10,30 @@ import '@/aws/config';
 
 // ===== HOME PAGE =====
 export default function Home() {
-  // Set state to see if the user is authenticated and store the user details in the state
-  const [userDetails, setUserDetails] = useAtom(userAtom);
+  const { user } = useAuth();
   const [loading, setLoading] = useState(true);
 
-  // Get user state from useAuth hook
-  const { user } = useAuth();
-
-  // Set state according to user authentication status
+  // Set loading state according to user authentication status
   useEffect(() => {
-    user &&
-      setUserDetails({
-        isLoggedIn: true,
-        user: user,
-      });
+    if (user !== undefined) {
+      setLoading(false);
+    }
+  }, [user]);
 
-    setLoading(false);
-  }, [setUserDetails, user]);
+  // Show loading spinner if authentication status is being determined
+  if (loading) {
+    return (
+      <div className="absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2">
+        <Spinner label="Loading..." size="lg" />
+      </div>
+    );
+  }
 
+  // Show different homepages based on user authentication status
   return (
     <div>
       <Nav />
-      {loading ? (
-        <div className="absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2">
-          <Spinner label="Loading..." size="lg" />
-        </div>
-      ) : userDetails.isLoggedIn ? (
-        <LoggedInHomepage />
-      ) : (
-        <BasicHomepage />
-      )}
+      {user ? <LoggedInHomepage /> : <BasicHomepage />}
     </div>
   );
 }
